01ecc529b808327c0e3cc6d80480124fa51d4587
jacobborga <jdborga@gmail.com>
2021-06-15 23:45:07 +0000

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, adjective) {
+    const cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards[i] = `Thank you, ${names[i]}, for the wonderful ${adjective} gift!`;
+    }
+    return cards;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n -= 1;
+    }
+}
